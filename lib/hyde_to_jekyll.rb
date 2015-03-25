6d18b5834124f90bec1c53f2f352b0d99de4e1fb
Robert H Grayson II <bobbygrayson@gmail.com>
2015-03-24 20:58:38 -0400

    working POC

--- lib/hyde_to_jekyll.rb
+++ lib/hyde_to_jekyll.rb
@@ -1,13 +1,29 @@
 require 'pry'
+require 'reverse_markdown'
 
 module HydeToJekyll
+
+  class Post
+    def initialize(klass=Class)
+      @posts = klass
+      @attributes = []
+    end
+
+    def add_field(title, content)
+      @attributes << [title, content]
+    end
+
+    def attributes
+      @attributes
+    end
+  end
+
   class MarkdownConverter
     def initialize
-      @converter = ReverseMarkdown
     end
 
     def convert(html, github_flavored=false)
-      @converter.convert(html, github_flavored: true)
+      ReverseMarkdown.convert(html, github_flavored: true)
     end
   end
 
@@ -18,15 +34,38 @@ module HydeToJekyll
     def setup
       `wget https://github.com/necolas/jekyll-boilerplate/archive/master.zip`
       `unzip master.zip`
+      `mkdir _posts`
       `ls`
     end
 
     def convert html
       @converter.convert html
     end
+
+    def write_posts markdown_posts
+      markdown_posts.map { |p|
+        File.open("_posts/#{p[1] + Random.rand(1000).to_s}.md", "wb") do |f|
+          f.write(p[0])
+        end
+      }
+    end
   end
 end
 
-a = HydeToJekyll::JekyllBlog.new
+def test
+  posts = [] 
+  1000.times do |time| 
+    posts << "<h1><div>#{time}</div></h1>"
+  end
+  converted_posts = []
+  blog = HydeToJekyll::JekyllBlog.new
+  posts.each do |post|
+    p = HydeToJekyll::Post.new
+    p.add_field("title#{Random.rand(1000).to_s}", post)
+    result = blog.convert p.attributes[0][1]
+    converted_posts << [result, p.attributes[0][0]]
+  end
+  blog.write_posts converted_posts
+end
 
-a.setup
+test
